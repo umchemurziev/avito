c6125693aa00c2d933b37e32c7d94b7bd12a8288
umchemurziev <umchemurziev@edu.hse.ru>
2021-09-10 17:53:44 +0300

    commented test

--- main.py
+++ main.py
@@ -53,17 +53,17 @@ async def get_matrix(url):
     read = await read_matrix(matrix)
     return read
 
-SOURCE_URL = 'https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt'
-TRAVERSAL = [
-    10, 50, 90, 130,
-    140, 150, 160, 120,
-    80, 40, 30, 20,
-    60, 100, 110, 70,
-]
+# SOURCE_URL = 'https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt'
+# TRAVERSAL = [
+#     10, 50, 90, 130,
+#     140, 150, 160, 120,
+#     80, 40, 30, 20,
+#     60, 100, 110, 70,
+# ]
 
 
-def test_get_matrix():
-    assert asyncio.run(get_matrix(SOURCE_URL)) == TRAVERSAL
+# def test_get_matrix():
+#     assert asyncio.run(get_matrix(SOURCE_URL)) == TRAVERSAL
 
 
-test_get_matrix()
+# test_get_matrix()
